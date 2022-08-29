2928a2897915586b3ac4807b148579a6df8ae59c
JaimeAlfonsoR <j.alfonsor@uniandes.edu.co>
2022-08-28 23:32:19 -0500

    instrucciones

--- proyecto0.py
+++ proyecto0.py
@@ -5,6 +5,9 @@ import nltk as tk
 comandos=["M","R","C","B","c","b","P","J","G"]                    #definicion de grupos
 instrucciones=["walk","jump","jumpTo","veer","look","drop","grab","get","free","pop","walk", "PROC", "CORP"]     
 condiciones=["isfacing","isValid","canWalk","not"]
+direcciones1=["left","right","around"]
+direcciones2=["left","right","front","back"]
+card=["north","south","east","west"]
 variables={"x":4}
 procedimientos = []
 
@@ -54,9 +57,123 @@ def explorarparam(num:int, tokens:list,i:int):
                 i-=1
                 errorSintax()    
                 error=True
+        elif num==1:
+            errorSintax()    
+            error=True        
         else:
             i+=1  
+            q+=1
     return i,error
+
+def evalins (tokens:list,i:int,error:bool):
+    if tokens[i]==";":
+        i+=1
+        error=True
+        errorSintax()
+    elif tokens[i]=="(":
+         
+        i+=1
+        v=str(tokens[i])
+        param=[]
+        p=""
+        for e in v:
+            if e!="," and e!=" " and e!=")":
+                p+=e
+            else:
+                if p!="":
+                    param.append(p) 
+                    p=""
+        if p!="":
+            param.append(p)        
+
+        print (param) 
+
+                        
+        if len(param)==2 :  
+            if tokens[i-2]=="jumpTo":
+
+                for k in param:
+                    if not(k.isdigit()) and not((k in variables)):
+                        errorSintax()   
+                        error=True
+                    elif (("." or "-") in k) and (k not in variables):
+                        errorSintax()   
+                        error=True
+                    elif (k in variables):
+                        if not(isinstance(variables[k], int)):
+                            errorSintax()   
+                            error=True
+            elif tokens[i-2]=="walk":
+
+                if not(param[1].isdigit()) and not((param[1] in variables)):
+                    errorSintax()   
+                    error=True
+                elif (("." or "-") in param[1]) and (param[1] not in variables):
+                    errorSintax()   
+                    error=True
+                elif (param[1] in variables):
+                    if not(isinstance(variables[param[0]], int)):
+                        errorSintax()   
+                        error=True 
+                if not(param[0] in direcciones2) and not((param[0] in card)):
+                    errorSintax()   
+                    error=True                                  
+            i+=1          
+        elif 0==len(param) or len(param)>=2:
+            errorSintax()
+            error=True
+
+        else: 
+            if tokens[i-2]=="jumpTo":
+                i,error=explorarparam(2,tokens,i)
+            elif  tokens[i-2]=="veer":   
+                if not(tokens[i] in direcciones1):
+                    errorSintax()   
+                    error=True
+                else:
+                    i+=1
+            elif  tokens[i-2]=="look":   
+                if not(tokens[i] in card):
+                    errorSintax()   
+                    error=True
+                else:
+                    i+=1 
+            elif tokens[i-2]=="walk": 
+                if tokens[i].isdigit() or (tokens[i] in variables):
+                    if ("." or "-") not in tokens[i] and tokens[i].isdigit():
+                        i+=1
+                    elif isinstance(variables[tokens[i]], int) and (tokens[i] in variables):
+                        i+=1
+                    else:
+                        errorSintax()
+                        error=True    
+                elif  (tokens[i] in direcciones2) or (tokens[i] in card):
+                    i+=1
+                    i,error=explorarparam(2,tokens,i)   
+                    print(33)     
+                else:
+                    errorSintax()
+                    error=True
+            else:
+                i,error=explorarparam(1,tokens,i)   
+        if tokens[i]==")":
+            i+=1
+                      
+            if tokens[i]==";":
+                i+=1
+            else:
+                errorSintax()
+                error=True 
+        else:
+            errorSintax()
+            error=True        
+                                         
+    else:
+        errorSintax()
+        error=True             
+
+    return i,error
+
 
 
 def parser(tokens):
@@ -127,7 +244,7 @@ def parser(tokens):
 
                         else: 
                             if tokens[i-2]=="G":
-                                i,error=explorarparam(2,tokens,i)
+                                i,error=explorarparam(3,tokens,i)
                                 
                             else:
                                 i,error=explorarparam(1,tokens,i)
@@ -187,6 +304,12 @@ def parser(tokens):
                         
                         else:
                             error = True
+            elif tokens[i] in instrucciones:
+                i+=1
+                print(tokens)
+                i,error=evalins(tokens,i,error)
+                
+
            
             else:
                 errorSintax()
